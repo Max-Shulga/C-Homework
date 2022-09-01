50a760f1156d4ef30d790b4cf96ed29379804766
Max Shulga <shulgamackim@gmail.com>
2022-09-01 12:39:22 +0300

    lesson 8 added

--- Lesson_7/Task_2/Program.cs
+++ Lesson_7/Task_2/Program.cs
@@ -10,7 +10,7 @@ int GetDigitCondition(string message)
 {
     Console.WriteLine(message);
     int colum = int.Parse(Console.ReadLine()!);
-    while(colum < 0)
+    while (colum < 0)
     {
         Console.Write("Invalid digit, please enter the correct digit: ");
         int temp = int.Parse(Console.ReadLine()!);
@@ -26,45 +26,44 @@ int GetDigit(string message)
     return start;
 }
 
-double [,] CreateMultiDouble(int minNum, int MaxNum)
+double[,] CreateMultiDouble(int minNum, int MaxNum)
 {
-    double [,] multi = new double[10, 10];
+    double[,] multi = new double[10, 10];
     for (int i = 0; i < multi.GetLength(0); i++)
     {
         for (int j = 0; j < multi.GetLength(1); j++)
         {
             Random digit = new Random();
-            multi[i,j] = digit.NextDouble()*(MaxNum - minNum) + minNum;
+            multi[i, j] = digit.NextDouble() * (MaxNum - minNum) + minNum;
         }
-    
     }
     return multi;
 }
 
-void PrintMultiDouble (double [,] multi)
+void PrintMultiDouble(double[,] multi)
 {
     for (int i = 0; i < multi.GetLength(0); i++)
     {
         for (int j = 0; j < multi.GetLength(1); j++)
         {
-            if(multi[i,j] == Math.Round(multi[i,j]))
+            if (multi[i, j] == Math.Round(multi[i, j]))
             {
                 Console.Write($"{multi[i, j]}\t ");
             }
-            else                                   
-            {                                                     
-               Console.Write($"{multi[i, j]:f2}\t ");
+            else
+            {
+                Console.Write($"{multi[i, j]:f2}\t ");
             }
         }
         Console.WriteLine();
     }
 }
 
-void FindValue(double [,] multi, int row, int colum)
+void FindValue(double[,] multi, int row, int colum)
 {
     try
     {
-        Console.WriteLine($"cell [{row}], [{colum}] contains the value: {multi[row,colum]:2f}");
+        Console.WriteLine($"cell [{row}], [{colum}] contains the value: {multi[row, colum]:2f}");
     }
     catch
     {
@@ -74,24 +73,35 @@ void FindValue(double [,] multi, int row, int colum)
 
 //мне прям очень понравилось верхнее решение, но на всякий и нижнее оставлю тут
 
-void FindValueClassic(double [,] multi, int row, int colum)
-{    
+void FindValueClassic(double[,] multi, int row, int colum)
+{
     double result = -1;
-    if(row > multi.GetLength(0) || colum > multi.GetLength(1))
+    if (row > multi.GetLength(0) || colum > multi.GetLength(1))
     {
         Console.Write("There is no such position in the array");
     }
     else
     {
-       Console.WriteLine($"cell [{row}], [{colum}] contains the value: {multi[row,colum]:2f}");
+        Console.WriteLine($"cell [{row}], [{colum}] contains the value: {multi[row, colum]:2f}");
     }
-    result = multi[row,colum];
+    result = multi[row, colum];
 }
 
 //------------------------------------------------------------------------------------------------
 
 Console.Clear();
-double[,] matrix = CreateMultiDouble(GetDigit("Input numerical start of range: : "),GetDigit("Input numerical end of range: : "));
+double[,] matrix = CreateMultiDouble(
+    GetDigit("Input numerical start of range: : "),
+    GetDigit("Input numerical end of range: : ")
+);
 PrintMultiDouble(matrix);
-FindValue(matrix, GetDigitCondition("Input the value of the row to search in the array: "), GetDigitCondition("Input the value of the colum to search in the array: "));
-FindValueClassic(matrix, GetDigitCondition("Input the value of the row to search in the array: "), GetDigitCondition("Input the value of the colum to search in the array: "));
+FindValue(
+    matrix,
+    GetDigitCondition("Input the value of the row to search in the array: "),
+    GetDigitCondition("Input the value of the colum to search in the array: ")
+);
+FindValueClassic(
+    matrix,
+    GetDigitCondition("Input the value of the row to search in the array: "),
+    GetDigitCondition("Input the value of the colum to search in the array: ")
+);
--- Lesson_7/Task_3/Program.cs
+++ Lesson_7/Task_3/Program.cs
@@ -9,7 +9,7 @@ int GetDigitCondition(string message)
 {
     Console.WriteLine(message);
     int colum = int.Parse(Console.ReadLine()!);
-    while(colum < 1)
+    while (colum < 1)
     {
         Console.Write("Invalid digit, please enter the correct digit: ");
         int temp = int.Parse(Console.ReadLine()!);
@@ -32,13 +32,13 @@ int[,] CreateMulti(int row, int colum, int minNum, int MaxNum)
     {
         for (int j = 0; j < multi.GetLength(1); j++)
         {
-            multi[i,j] = new Random().Next(minNum, MaxNum+1);
+            multi[i, j] = new Random().Next(minNum, MaxNum + 1);
         }
     }
     return multi;
 }
 
-void PrintMulti (int [,] multi)
+void PrintMulti(int[,] multi)
 {
     for (int i = 0; i < multi.GetLength(0); i++)
     {
@@ -48,25 +48,27 @@ void PrintMulti (int [,] multi)
         }
         Console.WriteLine();
     }
-
 }
 
-void AverageInColum(int [,] multi)
+void AverageInColum(int[,] multi)
 {
     double sum = 0;
-    for (int i = 0; i < multi.GetLength(1); i++)                           //перебор стобцов
+    for (int i = 0; i < multi.GetLength(1); i++) //перебор стобцов
     {
-        for (int j = 0; j < multi.GetLength(0); j++)                       //строк
+        for (int j = 0; j < multi.GetLength(0); j++) //строк
         {
-            sum += multi[j,i];                                            // считает сумму значений в стобце
+            sum += multi[j, i]; // считает сумму значений в стобце
         }
-    Console.WriteLine($" in {i+1} colum average is: {sum/multi.GetLength(0)}");      //делим на количество строк
-    sum = 0;
+        Console.WriteLine($" in {i + 1} colum average is: {sum / multi.GetLength(0)}"); //делим на количество строк
+        sum = 0;
     }
 }
 
-int[,] matrix = CreateMulti(GetDigitCondition("Input number of row for Multidimensional Arrays:  "),
-GetDigitCondition("\nInput number of colums for Multidimensional Arrays: "),
-GetDigit("\nInput numerical start of range: "),GetDigit("\nInput numerical end of range: "));
+int[,] matrix = CreateMulti(
+    GetDigitCondition("Input number of row for Multidimensional Arrays:  "),
+    GetDigitCondition("\nInput number of colums for Multidimensional Arrays: "),
+    GetDigit("\nInput numerical start of range: "),
+    GetDigit("\nInput numerical end of range: ")
+);
 PrintMulti(matrix);
 AverageInColum(matrix);
