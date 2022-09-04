7cb0d52ade4ee7099a67cd0b82803ef83b3b7b23
Max Shulga <shulgamackim@gmail.com>
2022-09-04 16:28:29 +0300

    lesson 9 added

---
+++ Lesson_9/Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+string ShowInterval(int a, int b)
+{
+    if(a == b) return $"{a}.";
+    else return $"{a}, {ShowInterval(a + 1, b)}";
+}
+
+Console.Write(ShowInterval(1,10));
+
+
